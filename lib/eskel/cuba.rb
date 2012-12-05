dd0b94bf947db8902e2ea5a83088ddfbc203d54f
Damian Caruso <damian.caruso@gmail.com>
2012-12-05 11:42:15 -0300

    add not_found helper

--- lib/eskel/cuba.rb
+++ lib/eskel/cuba.rb
@@ -3,6 +3,12 @@ module Eskel
     def redirect(*args)
       run ::Cuba.new { on(default) { res.redirect(*args) }}
     end
+
+    def not_found(msg = nil)
+      res.status = 404
+      res.write msg
+      halt(res.finish)
+    end
     
     def settings
       Eskel.settings
